078a72d9cb77e99ae5985cbd5e21fa5a4459f2e2
Javeriya Saeed Abbasi <jiyasaeed333@gmail.com>
2023-01-13 15:13:21 +0500

    scroll added

--- assets/js/customjs.js
+++ assets/js/customjs.js
@@ -4,76 +4,76 @@
 
 new WOW().init();
 
-function init(){
-	 new SmoothScroll(document,150,30)
+function init() {
+  new SmoothScroll(document, 15, 30);
 }
 
 function SmoothScroll(target, speed, smooth) {
-	if (target === document)
-		target = (document.scrollingElement 
-              || document.documentElement 
-              || document.body.parentNode 
-              || document.body) // cross browser support for document scrolling
-      
-	var moving = false
-	var pos = target.scrollTop
-  var frame = target === document.body 
-              && document.documentElement 
-              ? document.documentElement 
-              : target // safari is the new IE
-  
-	target.addEventListener('mousewheel', scrolled, { passive: true })
-	target.addEventListener('DOMMouseScroll', scrolled, { passive: true })
+  if (target === document)
+    target =
+      document.scrollingElement ||
+      document.documentElement ||
+      document.body.parentNode ||
+      document.body; // cross browser support for document scrolling
 
-	function scrolled(e) {
-  //   if(!moving) {
-  //     pos = target.scrollTop;
-  // }
-		e.preventDefault(); // disable default scrolling
+  var moving = false;
+  var pos = target.scrollTop;
+  var frame =
+    target === document.body && document.documentElement
+      ? document.documentElement
+      : target; // safari is the new IE
+  target.addEventListener("scroll", scrolled, { passive: true });
+  target.addEventListener("mousewheel", scrolled, { passive: false });
+  target.addEventListener("DOMMouseScroll", scrolled, { passive: false });
 
-		var delta = normalizeWheelDelta(e)
+  function scrolled(e) {
+    //   if(!moving) {
+    //     pos = target.scrollTop;
+    // }
+    e.preventDefault(); // disable default scrolling
 
-		pos += -delta * speed
-		pos = Math.max(0, Math.min(pos, target.scrollHeight - frame.clientHeight)) // limit scrolling
+    var delta = normalizeWheelDelta(e);
 
-		if (!moving) update()
-	}
+    pos += -delta * speed;
+    pos = Math.max(0, Math.min(pos, target.scrollHeight - frame.clientHeight)); // limit scrolling
 
-	function normalizeWheelDelta(e){
-		if(e.detail){
-			if(e.wheelDelta)
-				return e.wheelDelta/e.detail/40 * (e.detail>0 ? 1 : -1) // Opera
-			else
-				return -e.detail/3 // Firefox
-		}else
-			return e.wheelDelta/120 // IE,Safari,Chrome
-	}
+    if (!moving) update();
+  }
 
-	function update() {
-		moving = true
-    
-		var delta = (pos - target.scrollTop) / smooth
-    
-		target.scrollTop += delta
-    
-		if (Math.abs(delta) > 0.5)
-			requestFrame(update)
-		else
-			moving = false
-	}
+  function normalizeWheelDelta(e) {
+    if (e.detail) {
+      if (e.wheelDelta)
+        return (
+          (e.wheelDelta / e.detail / 40) * (e.detail > 0 ? 1 : -1)
+        ); // Opera
+      else return -e.detail / 3; // Firefox
+    } else return e.wheelDelta / 120; // IE,Safari,Chrome
+  }
 
-	var requestFrame = function() { // requestAnimationFrame cross browser
-		return (
-			window.requestAnimationFrame ||
-			window.webkitRequestAnimationFrame ||
-			window.mozRequestAnimationFrame ||
-			window.oRequestAnimationFrame ||
-			window.msRequestAnimationFrame ||
-			function(func) {
-				window.setTimeout(func, 1000 / 50);
-			}
-		);
-	}()
+  function update() {
+    moving = true;
+
+    var delta = (pos - target.scrollTop) / smooth;
+
+    target.scrollTop += delta;
+
+    if (Math.abs(delta) > 0.5) requestFrame(update);
+    else moving = false;
+  }
+
+  var requestFrame = (function () {
+    // requestAnimationFrame cross browser
+    return (
+      window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function (func) {
+        window.setTimeout(func, 1000 / 50);
+      }
+    );
+  })();
 }
 
 //--- Jquery scroll:
@@ -82,11 +82,10 @@ function SmoothScroll(target, speed, smooth) {
 //   jQuery.scrollSpeed(200, 800);
 // });
 
-
 // (function($) {
 
 // jQuery.scrollSpeed = function(step, speed, easing) {
-    
+
 //     var $document = $(document),
 //         $window = $(window),
 //         $body = $('html, body'),
@@ -96,78 +95,78 @@ function SmoothScroll(target, speed, smooth) {
 //         scrollY,
 //         scrollX,
 //         view;
-        
+
 //     if (window.navigator.msPointerEnabled)
-    
+
 //         return false;
-        
+
 //     $window.on('mousewheel DOMMouseScroll', function(e) {
-        
+
 //         var deltaY = e.originalEvent.wheelDeltaY,
 //             detail = e.originalEvent.detail;
 //             scrollY = $document.height() > $window.height();
 //             scrollX = $document.width() > $window.width();
 //             scroll = true;
-        
+
 //         if (scrollY) {
-            
+
 //             view = $window.height();
-                
+
 //             if (deltaY < 0 || detail > 0)
-        
+
 //                 root = (root + view) >= $document.height() ? root : root += step;
-            
+
 //             if (deltaY > 0 || detail < 0)
-        
+
 //                 root = root <= 0 ? 0 : root -= step;
-            
+
 //             $body.stop().animate({
-        
+
 //                 scrollTop: root
-            
+
 //             }, speed, option, function() {
-        
+
 //                 scroll = false;
-            
+
 //             });
 //         }
-        
+
 //         if (scrollX) {
-            
+
 //             view = $window.width();
-                
+
 //             if (deltaY < 0 || detail > 0)
-        
+
 //                 root = (root + view) >= $document.width() ? root : root += step;
-            
+
 //             if (deltaY > 0 || detail < 0)
-        
+
 //                 root = root <= 0 ? 0 : root -= step;
-            
+
 //             $body.stop().animate({
-        
+
 //                 scrollLeft: root
-            
+
 //             }, speed, option, function() {
-        
+
 //                 scroll = false;
-            
+
 //             });
 //         }
-        
+
 //         return false;
-        
+
 //     }).on('scroll', function() {
-        
+
 //         if (scrollY && !scroll) root = $window.scrollTop();
 //         if (scrollX && !scroll) root = $window.scrollLeft();
-        
+
 //     }).on('resize', function() {
-        
+
 //         if (scrollY && !scroll) view = $window.height();
 //         if (scrollX && !scroll) view = $window.width();
-        
-//     });       
+
+//     });
 // };
 
 // jQuery.easing.default = function (x,t,b,c,d) {
@@ -176,9 +175,14 @@ function SmoothScroll(target, speed, smooth) {
 // };
 
 // })(jQuery);
-
-
-
+$('.menus a').click(function (e) {
+  var linkHref = $(this).attr("href");
+  var idElement = linkHref.substr(linkHref.indexOf("#"));
+  $('html, body').animate({
+      scrollTop: $(idElement).offset().top
+  }, 2000);
+  return false;
+});
 //--- Scroll to specific values
 // scrollTo is the same
 window.scrollTo({
@@ -246,7 +250,7 @@ window.addEventListener("scroll", function () {
   mainRightPage2.style.top = 750 + "px";
   // PAGE 3
   page3.style.top = 1530 + "px";
-  backgroundpage3.style.transform="translateY(" + value * 0.1 + "px) ";
+  backgroundpage3.style.transform = "translateY(" + value * 0.1 + "px) ";
   mountains1.style.transform = "translateY(" + value * 0.058 + "px) ";
   // mountains1.style.top = 26 + "px";
   mountains2.style.transform = "translateY(" + value * 0.09 + "px) ";
@@ -257,7 +261,7 @@ window.addEventListener("scroll", function () {
   page4.style.top = 2692 + "px";
   background1Page4.style.transform = "translateY(" + value * -0.005 + "px)";
   background2Page4.style.transform = "translateY(" + value * -0.015 + "px)";
-  background3Page4.style.transform = "translateY(" + value * -0.030 + "px)";
+  background3Page4.style.transform = "translateY(" + value * -0.03 + "px)";
   // wavePage4.style.transform = "translateY(" + value * 0.3 + "px)";\
   wave1.style.transform = "translateX(-" + value * 0.05 + "px)";
   wave2.style.transform = "translateX(" + value * 0.003 + "px)";
@@ -270,7 +274,6 @@ window.addEventListener("scroll", function () {
 //   console.log(wave1);
 // });
 
-
 // var leftItem = document.getElementById('wave1'),
 //     rightItem = document.getElementById('wave2');
 
@@ -279,15 +282,12 @@ window.addEventListener("scroll", function () {
 //   rightItem.style.transform = "translateX(" + window.pageYOffset + "deg)";
 // })
 
-
 // var scrollPos = window.pageYOffset;
 // console.log(scrollPos);
 // document.querySelector("body").scrollHeight
 // document.scrollingElement.offsetHeight
 // var offset = window.pageYOffset/(document.querySelector("body").scrollHeight - document.scrollingElement.offsetHeight);
 // console.log(offset);
-
-
 
 // var prevScrollpos = window.pageYOffset
 // $(window).scroll(function() {
@@ -324,14 +324,24 @@ window.addEventListener("scroll", function () {
 //App menu hover
 $(".menu.apps").hover(
   function over() {
-    $(".apps .over").css({opacity: "1" });
-    $(".apps .background").css({width: "72px" , height: "34px" ,marginLeft:"0px",marginTop:"0px",marginRight:"0px"});
+    $(".apps .over").css({ opacity: "1" });
+    $(".apps .background").css({
+      width: "72px",
+      height: "34px",
+      marginLeft: "0px",
+      marginTop: "0px",
+      marginRight: "0px",
+    });
     $(".apps .over").addClass("menuTransitionAnimation");
     console.log("mouseover");
   },
   function out() {
-    $(".apps .over").css({opacity: "0" });
-    $(".apps .over .background").css({width: "0px" , height: "0px" ,marginLeft:"0px"});
+    $(".apps .over").css({ opacity: "0" });
+    $(".apps .over .background").css({
+      width: "0px",
+      height: "0px",
+      marginLeft: "0px",
+    });
     $(".apps .over").addClass("menuTransitionAnimation");
     console.log("mouseout");
   }
@@ -339,14 +349,24 @@ $(".menu.apps").hover(
 //Home menu hover
 $(".menu.home").hover(
   function over() {
-    $(".home .over").css({opacity: "1" });
-    $(".home .background").css({width: "35px" , height: "34px" ,marginLeft:"0px",marginTop:"0px",marginRight:"0px"});
+    $(".home .over").css({ opacity: "1" });
+    $(".home .background").css({
+      width: "35px",
+      height: "34px",
+      marginLeft: "0px",
+      marginTop: "0px",
+      marginRight: "0px",
+    });
     $(".home .over").addClass("menuTransitionAnimation");
     console.log("mouseover");
   },
   function out() {
-    $(".home .over").css({opacity: "0" });
-    $(".home .over .background").css({width: "0px" , height: "0px" ,marginLeft:"0px"});
+    $(".home .over").css({ opacity: "0" });
+    $(".home .over .background").css({
+      width: "0px",
+      height: "0px",
+      marginLeft: "0px",
+    });
     $(".home .over").addClass("menuTransitionAnimation");
     console.log("mouseout");
   }
@@ -354,14 +374,24 @@ $(".menu.home").hover(
 //About menu hover
 $(".menu.about").hover(
   function over() {
-    $(".about .over").css({opacity: "1" });
-    $(".about .background").css({width: "88px" , height: "34px" ,marginLeft:"0px",marginTop:"0px",marginRight:"0px"});
+    $(".about .over").css({ opacity: "1" });
+    $(".about .background").css({
+      width: "88px",
+      height: "34px",
+      marginLeft: "0px",
+      marginTop: "0px",
+      marginRight: "0px",
+    });
     $(".about .over").addClass("menuTransitionAnimation");
     console.log("mouseover");
   },
   function out() {
-    $(".about .over").css({opacity: "0" });
-    $(".about .over .background").css({width: "0px" , height: "0px" ,marginLeft:"0px"});
+    $(".about .over").css({ opacity: "0" });
+    $(".about .over .background").css({
+      width: "0px",
+      height: "0px",
+      marginLeft: "0px",
+    });
     $(".about .over").addClass("menuTransitionAnimation");
     console.log("mouseout");
   }
@@ -369,14 +399,24 @@ $(".menu.about").hover(
 //Contact menu hover
 $(".menu.contact").hover(
   function over() {
-    $(".contact .over").css({opacity: "1" });
-    $(".contact .background").css({width: "112px" , height: "34px" ,marginLeft:"0px",marginTop:"0px",marginRight:"0px"});
+    $(".contact .over").css({ opacity: "1" });
+    $(".contact .background").css({
+      width: "112px",
+      height: "34px",
+      marginLeft: "0px",
+      marginTop: "0px",
+      marginRight: "0px",
+    });
     $(".contact .over").addClass("menuTransitionAnimation");
     console.log("mouseover");
   },
   function out() {
-    $(".contact .over").css({opacity: "0" });
-    $(".contact .over .background").css({width: "0px" , height: "0px" ,marginLeft:"0px"});
+    $(".contact .over").css({ opacity: "0" });
+    $(".contact .over .background").css({
+      width: "0px",
+      height: "0px",
+      marginLeft: "0px",
+    });
     $(".contact .over").addClass("menuTransitionAnimation");
     console.log("mouseout");
   }
